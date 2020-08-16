312ee619080f7733e3ae897769468881069e1de5
Jeremy Ong <jeremycong@gmail.com>
2020-08-16 00:43:20 -0600

    Add issue creation

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const context_payload = JSON.stringify(github.context.payload, undefined, 2);
-console.log(`Context payload: ${context_payload}`);
+const { id, message, owner, repo, user } = github.context.payload.inputs;
 
-const event_payload = JSON.stringify(github.event, undefined, 2);
-console.log(`Context payload: ${event_payload}`);
+const token = core.getInput('bot_token');
+const octokit = github.getOctokit(token);
+
+const decoded = Buffer.from(message, 'base64').toString('utf8');
+
+octokit.issues.create({
+    owner,
+    repo,
+    title: 'More eyes, plz!',
+    body: decoded,
+}).then(value => {
+    console.log('Issue created: ', value.data);
+    octokit.repos.getAllTopics({
+        owner,
+        repo
+    }).then((value) => {
+        const labels = value.names;
+        const commit_url = `https://github.com/${owner}/${repo}/commit/${id}`;
+        octokit.issues.create({
+            owner: 'moreeyesplz',
+            repo: 'meeper',
+            title: commit_url,
+            body: `${user} has requested more eyes [here](${commit_url}).\n\n${decoded}`,
+            labels,
+        }).catch((e) => {
+            console.log('Failed to make meeper issue: ', e);
+        })
+    }).catch((e) => {
+        console.log('Failed to fetch repo topics: ', e);
+    });
+}).catch(e => {
+    console.log('Failed to make user issue: ', e);
+});
